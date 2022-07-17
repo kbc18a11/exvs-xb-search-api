88affc6b642702744f4cf30a98bec2cc65891812
kbc18a11 <wada121400@gmail.com>
2022-07-17 14:23:06 +0900

    .gitignoreの修正

---
+++ src/batchs/db/db_migration.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/GIT_USER_ID/GIT_REPO_ID/src/common"
+	"github.com/GIT_USER_ID/GIT_REPO_ID/src/models"
+)
+
+func main() {
+	db, err := common.DbConnectionInit()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(
+		&models.TitleOfWork{},
+		&models.Pilot{},
+		&models.AirframeCost{},
+		&models.AwakenType{},
+		&models.Airframe{},
+	)
+
+}
